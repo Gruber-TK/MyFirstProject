6b2e438e35b7690e960c24f10771e2d08b022d63
Theresa Gruber <112355810+Gruber-TK@users.noreply.github.com>
2022-09-04 23:07:02 -0500

    first commit being made

---
+++ src/Application.java
@@ -0,0 +1,30 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is a comment 
+		
+		
+		//Varialbe declaration -type,identifies, assignment operator, value, semicolon
+		int age = 34;
+		System.out.println(age);
+		boolean isAge30 = age == 30;
+		System.out.println(isAge30);
+		
+		System.out.println(34 == 34);
+		
+		age = age * 2;
+		System.out.println(age);
+		
+		String firstName = "Tommy";
+		String lastName = "Smith";
+		String fullName = firstName + " " + lastName;
+		System.out.println(fullName);
+				
+		
+		
+		
+	}
+
+}
